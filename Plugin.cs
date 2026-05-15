4d50062d84b3b9545b63cef952b605cb81dee7bc
Maksym Hazevych <mhazevych@mailbox.org>
2026-05-15 20:46:39 +0300

    Include a formatter

--- Plugin.cs
+++ Plugin.cs
@@ -5,18 +5,16 @@ using BepInEx.Configuration;
 namespace ShowRaftCenterNG;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BaseUnityPlugin
-{
-    internal static new ManualLogSource Logger;
-    private ConfigEntry<string> keybind;
-        
-    private void Awake()
-    {
-        // Plugin startup logic
-        Logger = base.Logger;
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+public class Plugin : BaseUnityPlugin {
+	internal static new ManualLogSource Logger;
+	private ConfigEntry<string> keybind;
 
-        keybind = Config.Bind("General", "Keybind", "=", "The keybind used to show the center");
-        Logger.LogInfo($"Using '{keybind.Value}' for the keybind");
-    }
+	private void Awake() {
+		// Plugin startup logic
+		Logger = base.Logger;
+		Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+		keybind = Config.Bind("General", "Keybind", "=", "The keybind used to show the center");
+		Logger.LogInfo($"Using '{keybind.Value}' for the keybind");
+	}
 }
